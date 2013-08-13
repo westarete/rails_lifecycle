15c4ed7670c164fd09d91d51c73577a4f652caeb
Scott Woods <scott@westarete.com>
2013-08-12 21:37:44 -0400

    Install a stock rails 4 application
    Command line was:
    
        rails new . --skip-test-unit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RailsLifecycle::Application.config.secret_key_base = '2a1a4edc240de8f285c3870493820fcb24fd25b0bf02460215adfeeeee1025833fd1188fbaec5907b949ef4a9f46f05e6ef7f67eb080e274078b788a0792a43e'
