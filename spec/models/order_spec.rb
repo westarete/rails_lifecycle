f4c51915f844f71bef769ce988100f23427bffa3
Scott Woods <scott@westarete.com>
2013-08-14 13:56:39 -0400

    Fill in model attribute specs

--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -1,5 +1,9 @@
 describe Order do
 
+  it { should have_db_column(:name).of_type(:string) }
+  it { should have_db_column(:po_number).of_type(:string) }
+  it { should have_db_column(:terms).of_type(:string) }
+
   describe '.new' do
     it "supports mass assignment" do
       o = Order.new(name: "Bob", po_number: "100", terms: "Net 10")
